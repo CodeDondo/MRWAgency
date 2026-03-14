4386029557a363f1db36c19a0862b0b029250bda
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2026-03-14 01:00:03 +0100

    fixed

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/favicon.ico",
+        destination: "/mrw_agency_logo_round.png",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- my-app/src/app/favicon.ico/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET(request) {
-  const faviconUrl = new URL("/mrw_agency_logo_round.png", request.url);
-  return NextResponse.redirect(faviconUrl, 308);
-}
